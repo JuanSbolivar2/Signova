96b7176efb482c1d52479003427f96e4ab296065
JotaMoreno023 <jotamoreno629@gmail.com>
2026-07-26 14:07:45 -0500

    Update firebase.js

--- assets/firebase/firebase.js
+++ assets/firebase/firebase.js
@@ -1,1 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBki5ziLxT4HjggWizmvpbCYwQRSi60OME",
+  authDomain: "signova-2a450.firebaseapp.com",
+  projectId: "signova-2a450",
+  storageBucket: "signova-2a450.firebasestorage.app",
+  messagingSenderId: "208358580964",
+  appId: "1:208358580964:web:86ea2ffece55f0b69842f1",
+  measurementId: "G-SN3HZW9YLF"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Servicios que usaremos
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+console.log("✅ Firebase conectado correctamente");
